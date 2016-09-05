684c92186ab0ef313372be506ccf7f3bfe902746
徐培堯 <st89382000@gmail.com>
2016-09-05 15:27:56 +0800

    合併專案目錄並使用Face資料庫存取模組

---
+++ FaceDatabaseExplorer/Main.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FaceDatabaseExplorer {
+    partial class Main {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.關閉XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(796, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案FToolStripMenuItem
+            // 
+            this.檔案FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開啟OToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.關閉XToolStripMenuItem});
+            this.檔案FToolStripMenuItem.Name = "檔案FToolStripMenuItem";
+            this.檔案FToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.檔案FToolStripMenuItem.Text = "檔案(&F)";
+            // 
+            // 開啟OToolStripMenuItem
+            // 
+            this.開啟OToolStripMenuItem.Name = "開啟OToolStripMenuItem";
+            this.開啟OToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.開啟OToolStripMenuItem.Text = "開啟(&O)";
+            this.開啟OToolStripMenuItem.Click += new System.EventHandler(this.開啟OToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // 關閉XToolStripMenuItem
+            // 
+            this.關閉XToolStripMenuItem.Name = "關閉XToolStripMenuItem";
+            this.關閉XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.關閉XToolStripMenuItem.Text = "關閉(&X)";
+            this.關閉XToolStripMenuItem.Click += new System.EventHandler(this.關閉XToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(796, 266);
+            this.splitContainer1.SplitterDistance = 145;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(145, 266);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(647, 266);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 290);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "臉部辨識資料庫瀏覽器";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開啟OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 關閉XToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FaceDatabaseExplorer/Main.cs
@@ -0,0 +1,38 @@
+﻿using DF_FaceTracking.cs;
+using RealSenseSdkExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceDatabaseExplorer {
+    public partial class Main : Form {
+        public List<NameMapping> NameMapping = null;
+        public List<RecognitionFaceData> FaceData = null;
+        public Main() {
+            InitializeComponent();
+
+            #region 開啟檔案篩選
+            openFileDialog1.Filter = "Zip files (*.zip)|*.zip";
+            openFileDialog1.FileName = null;
+            openFileDialog1.Multiselect = false;
+            #endregion
+        }
+
+        private void 關閉XToolStripMenuItem_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void 開啟OToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            FaceDatabaseFile.Load(
+                openFileDialog1.FileName,
+                ref FaceData, ref NameMapping);
+        }
+    }
+}
